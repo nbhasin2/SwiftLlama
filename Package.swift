e83acd96b5da7471582d11129c3363f12b0be4a8
Nishant Bhasin <nish.bhasin@gmail.com>
2024-12-04 11:39:35 -0500

    iOS 15 changes

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.0
 
 import PackageDescription
 
@@ -6,7 +6,7 @@ let package = Package(
     name: "SwiftLlama",
     platforms: [
         .macOS(.v15),
-        .iOS(.v18),
+        .iOS(.v15),
         .watchOS(.v11),
         .tvOS(.v18),
         .visionOS(.v2)
